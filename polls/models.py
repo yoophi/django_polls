611ed98d4210d9d68bd4ea12bc1e7514e8fe067a
Pyunghyuk Yoo <yoophi@gmail.com>
2016-03-22 23:22:31 +0900

    tutorial 05 - added tests.

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-from datetime import datetime
+import datetime
 
 from django.db import models
 from django.utils import timezone
@@ -16,7 +16,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 @python_2_unicode_compatible
